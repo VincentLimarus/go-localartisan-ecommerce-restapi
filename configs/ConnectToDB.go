a0dc4672c458ac6335a3c541ddf82577b86b103f
Vincent Oliver Limarus <vincentlimarus27@gmail.com>
2024-07-24 01:07:59 +0700

    feat: first commit

---
+++ configs/ConnectToDB.go
@@ -0,0 +1,27 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Fail to connect to database")
+	}
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
