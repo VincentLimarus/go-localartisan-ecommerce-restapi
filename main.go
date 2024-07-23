638648be662f576bd2dcefd1dd69469308c25eeb
Vincent Oliver Limarus <vincentlimarus27@gmail.com>
2024-07-24 01:20:42 +0700

    gitignore testing

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "localArtisans/configs"
+
+func init() {
+	configs.LoadEnvVariables()
+	configs.ConnectToDB()
+}
+
+// @title Local Artisans API
+// @version 1.0
+// @description This is the API for Local Artisans
+// @BasePath /api/v1
+// @AuthPath /api/v1/auth
 func main() {
-	println("Hello, World!")
+	
 }
