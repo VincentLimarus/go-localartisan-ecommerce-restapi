eb454cd96d7ffc357b7cb42a15c8539a207f6f08
Vincent Oliver Limarus <vincentlimarus27@gmail.com>
2024-08-22 23:53:17 +0700

    fix database carts.go

--- models/database/carts.go
+++ models/database/carts.go
@@ -12,8 +12,8 @@ type Carts struct {
 	IsActive 	bool 		`gorm:"type:boolean;not null"`
 	CreatedBy 	string 		`gorm:"type:varchar(255);not null; default:'system'"`
 	UpdatedBy 	string 		`gorm:"type:varchar(255);not null; default:'system'"`
-	CreatedAt	time.Time 	`gorm:"type:timestamp;not null"; default:now()`
-	UpdatedAt 	time.Time 	`gorm:"type:timestamp;not null"; default:now()`
+	CreatedAt time.Time `gorm:"autoCreateTime;not null;default:now()"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime;not null;default:now()"`
 
 	// Start of References
 	CartInformation []CartInformations `gorm:"foreignKey:CartID"`
